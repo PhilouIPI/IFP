63efeb47d22eb64fa098aa3576a806f38d346cd5
PhilouIPI <philippe.naovarath@campus-igs-toulouse.fr>
2020-04-08 11:28:53 +0200

    correction persistence file

--- src/Hibernate/NewsTest.java
+++ src/Hibernate/NewsTest.java
@@ -1,16 +1,27 @@
 package Hibernate;
-
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 class NewsTest {
 
+    public static class MyEntityManagerFactory {
+        private static final EntityManagerFactory emFactory;
+        static {
+            emFactory = Persistence.createEntityManagerFactory("derby-persistence-unit");
+        }
+        public static EntityManager getEntityManager(){
+            return emFactory.createEntityManager();
+        }
+        public static void close(){
+            emFactory.close();
+        }
+    }
     @BeforeEach
     void setUp() {
     }
-
     @AfterEach
     void tearDown() {
     }
