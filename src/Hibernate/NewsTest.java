3869ae10dbdd8dadb26bccbd235cad1b28d77f79
PhilouIPI <philippe.naovarath@campus-igs-toulouse.fr>
2020-04-08 13:40:55 +0200

    test de connection

--- src/Hibernate/NewsTest.java
+++ src/Hibernate/NewsTest.java
@@ -2,11 +2,13 @@ package Hibernate;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
+
+import org.junit.Test;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 
 class NewsTest {
-
+    //@Test
     public static class MyEntityManagerFactory {
         private static final EntityManagerFactory emFactory;
         static {
---
+++ src/Hibernate/NewsTest2.java
@@ -0,0 +1,18 @@
+package Hibernate;
+import org.junit.jupiter.api.Test;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import static org.junit.jupiter.api.Assertions.*;
+
+class NewsTest2 {
+
+    @Test
+    void sessionOk() {
+        EntityManagerFactory sessionFactory = Persistence.createEntityManagerFactory("derby-persistence-unit");
+        EntityManager entityManager = sessionFactory.createEntityManager();
+        assertTrue(entityManager.isOpen());
+
+    }
+
+}
